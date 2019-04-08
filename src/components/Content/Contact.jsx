ef1206535c9f791dcf047dfc3978969ad5595fc6
Takumi Adachi <takumiadachi@gmail.com>
2019-04-08 06:44:15 -0700

    Add email as my preferred point of contact.

--- src/components/Content/Contact.jsx
+++ src/components/Content/Contact.jsx
@@ -3,9 +3,20 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 // Material-UI
 import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import IconButton from "@material-ui/core/IconButton";
+import EmailIcon from "@material-ui/icons/Email";
 
 // We can inject some CSS into the DOM.
-const styles = theme => ({});
+const styles = theme => ({
+  email: {
+    marginTop: 10
+  },
+  emailIconSize: {
+    fontSize: 30
+  }
+});
 
 function Contact(props) {
   const { classes, children, className, ...other } = props;
@@ -17,6 +28,27 @@ function Contact(props) {
       <Typography paragraph>
         This is where I will put my Contact Details.
       </Typography>
+      <Typography>
+        For now, my preferred point of contact is my email:
+      </Typography>
+
+      <FormControlLabel
+        className={classes.email}
+        control={
+          <a
+            target="_top"
+            rel="noopener noreferrer"
+            href="mailto:takumiadachi@gmail.com"
+            target="_blank"
+          >
+            <IconButton color="primary">
+              <EmailIcon className={classes.emailIconSize} /> {/* icon */}
+            </IconButton>
+          </a>
+        }
+        label={"takumiadachi@gmail.com"}
+        labelPlacement="end"
+      />
     </div>
   );
 }
