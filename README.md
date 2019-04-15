b54806280cf491912d794ab2a245b30ca950c95b
Takumi Adachi <takumiadachi@gmail.com>
2019-04-15 15:53:01 -0700

    Update README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Deployment Instructions
+## Deployment Instructions
 
 ```
 (npm run build)
@@ -8,7 +8,7 @@ OR
 (now rm homepage --f -y) -and (now -A now.json) -and (now -A now.json alias)
 ```
 
-Remove from zeit.co/now
+## Remove from zeit.co/now
 
 ```
 (now rm homepage --f -y)
--- src/components/Content/Contact.jsx
+++ src/components/Content/Contact.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 // Material-UI
 import Typography from "@material-ui/core/Typography";
-import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import IconButton from "@material-ui/core/IconButton";
 import EmailIcon from "@material-ui/icons/Email";
